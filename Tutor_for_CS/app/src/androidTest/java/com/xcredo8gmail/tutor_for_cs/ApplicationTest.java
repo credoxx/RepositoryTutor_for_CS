4f7b8f2ac7bf823ebc5b13bcba79397172886d4c
IVAN <xcredo8@gmail.com>
2016-05-05 17:29:34 +0600

    project

---
+++ Tutor_for_CS/app/src/androidTest/java/com/xcredo8gmail/tutor_for_cs/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.xcredo8gmail.tutor_for_cs;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
